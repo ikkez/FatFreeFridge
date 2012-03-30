644f7212e88114e2b48d33fa88da3b88982417d9
ikkez <ikkez0n3@gmail.com>
2012-03-30 16:59:47 +0200

    further work on details for set,get,clear, .. smooth scrolling added

--- js/main.js
+++ js/main.js
@@ -10,13 +10,32 @@ $(document).ready(function() {
 		});
 	});*/
 	
-	// popover	
+	/* PopOver
+	***********/
 	$("a[rel=popover]")
 	  .popover()
 	  .click(function(e) {
 	    e.preventDefault()
 	  });
 	  
+	
+	/* Smoothscroll
+	****************/
+	$('a[href*=#]').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		   var $target = $(this.hash);
+		   $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		   if ($target.length) {
+		       var targetOffset = $target.offset().top;
+		       $('html,body').animate({scrollTop: targetOffset-60}, 500);
+		       return false;
+		   }
+		}
+	});
+	   
+	
+	/* Hidden Sections
+	*******************/    
 	$('.btn-info').toggle(function(){
 		$(this).next('div.hidden').css({visibility: 'visible'}).slideDown();
 	},function(){
